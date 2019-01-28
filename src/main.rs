85e81d2ff0e6534165baecce681189dda3e1d34c
Sönke Hahn <soenkehahn@gmail.com>
2019-01-28 11:19:09 +0800

    format workspaces differently

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,61 @@
+use i3ipc::reply::{Node, NodeType};
 use i3ipc::I3Connection;
 use std::error::Error;
 
 fn main() -> Result<(), Box<Error>> {
     let mut connection = I3Connection::connect()?;
-    print!("{}", connection.get_tree()?.pretty());
+    print!("{}", connection.get_tree()?.custom_pretty(format_node));
     Ok(())
+}
+
+fn format_node(node: &Node) -> String {
+    let name = node.name.clone().unwrap_or_else(|| "<None>".to_string());
+    match node.nodetype {
+        NodeType::Workspace => format!("WORKSPACE: {}", name),
+        _ => name,
+    }
+}
+
+#[cfg(test)]
+mod format_node {
+    use super::*;
+
+    use i3ipc::reply::{NodeBorder, NodeLayout};
+
+    fn node() -> Node {
+        Node {
+            focus: vec![],
+            nodes: vec![],
+            floating_nodes: vec![],
+            id: 1,
+            name: None,
+            nodetype: NodeType::Con,
+            border: NodeBorder::Normal,
+            current_border_width: 4,
+            layout: NodeLayout::SplitH,
+            percent: None,
+            rect: (0, 0, 100, 100),
+            window_rect: (0, 0, 0, 0),
+            deco_rect: (0, 0, 0, 0),
+            geometry: (0, 0, 0, 0),
+            window: None,
+            urgent: false,
+            focused: false,
+        }
+    }
+
+    #[test]
+    fn uses_the_name() {
+        let mut node = node();
+        node.name = Some(format!("foo"));
+        assert_eq!(format_node(&node), "foo")
+    }
+
+    #[test]
+    fn marks_workspaces_as_such() {
+        let mut node = node();
+        node.name = Some(format!("foo"));
+        node.nodetype = NodeType::Workspace;
+        assert_eq!(format_node(&node), "WORKSPACE: foo")
+    }
 }
