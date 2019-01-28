0a784b336a7fa9ccbebea94d36a176e2e7b89596
Sönke Hahn <soenkehahn@gmail.com>
2019-01-28 23:35:44 +0800

    restructure test-suite

--- src/main.rs
+++ src/main.rs
@@ -36,66 +36,70 @@ fn format_node(node: &Node) -> String {
 mod format_node {
     use super::*;
 
-    use i3ipc::reply::{NodeBorder, NodeLayout};
+    mod format_node {
+        use super::*;
 
-    fn node() -> Node {
-        Node {
-            focus: vec![],
-            nodes: vec![],
-            floating_nodes: vec![],
-            id: 1,
-            name: None,
-            nodetype: NodeType::Con,
-            border: NodeBorder::Normal,
-            current_border_width: 4,
-            layout: NodeLayout::SplitH,
-            percent: None,
-            rect: (0, 0, 100, 100),
-            window_rect: (0, 0, 0, 0),
-            deco_rect: (0, 0, 0, 0),
-            geometry: (0, 0, 0, 0),
-            window: None,
-            urgent: false,
-            focused: false,
+        use i3ipc::reply::{NodeBorder, NodeLayout};
+
+        fn node() -> Node {
+            Node {
+                focus: vec![],
+                nodes: vec![],
+                floating_nodes: vec![],
+                id: 1,
+                name: None,
+                nodetype: NodeType::Con,
+                border: NodeBorder::Normal,
+                current_border_width: 4,
+                layout: NodeLayout::SplitH,
+                percent: None,
+                rect: (0, 0, 100, 100),
+                window_rect: (0, 0, 0, 0),
+                deco_rect: (0, 0, 0, 0),
+                geometry: (0, 0, 0, 0),
+                window: None,
+                urgent: false,
+                focused: false,
+            }
         }
-    }
 
-    #[test]
-    fn uses_the_name() {
-        let mut node = node();
-        node.name = Some(format!("foo"));
-        assert_eq!(format_node(&node), "foo")
-    }
+        #[test]
+        fn uses_the_name() {
+            let mut node = node();
+            node.name = Some(format!("foo"));
+            assert_eq!(format_node(&node), "foo")
+        }
 
-    #[test]
-    fn marks_workspaces_as_such() {
-        let mut node = node();
-        node.name = Some(format!("foo"));
-        node.nodetype = NodeType::Workspace;
-        assert_eq!(format_node(&node), "WORKSPACE: foo")
-    }
+        #[test]
+        fn marks_workspaces_as_such() {
+            let mut node = node();
+            node.name = Some(format!("foo"));
+            node.nodetype = NodeType::Workspace;
+            assert_eq!(format_node(&node), "WORKSPACE: foo")
+        }
 
-    #[test]
-    fn marks_outputs_as_such() {
-        let mut node = node();
-        node.name = Some(format!("foo"));
-        node.nodetype = NodeType::Output;
-        assert_eq!(format_node(&node), "OUTPUT: foo")
-    }
+        #[test]
+        fn marks_outputs_as_such() {
+            let mut node = node();
+            node.name = Some(format!("foo"));
+            node.nodetype = NodeType::Output;
+            assert_eq!(format_node(&node), "OUTPUT: foo")
+        }
 
-    #[test]
-    fn marks_root_as_such() {
-        let mut node = node();
-        node.name = Some(format!("root"));
-        node.nodetype = NodeType::Root;
-        assert_eq!(format_node(&node), "ROOT")
-    }
+        #[test]
+        fn marks_root_as_such() {
+            let mut node = node();
+            node.name = Some(format!("root"));
+            node.nodetype = NodeType::Root;
+            assert_eq!(format_node(&node), "ROOT")
+        }
 
-    #[test]
-    fn shows_layout_for_unnamed_containers() {
-        let mut node = node();
-        node.nodetype = NodeType::Con;
-        node.layout = NodeLayout::SplitH;
-        assert_eq!(format_node(&node), "CONTAINER: SplitH")
+        #[test]
+        fn shows_layout_for_unnamed_containers() {
+            let mut node = node();
+            node.nodetype = NodeType::Con;
+            node.layout = NodeLayout::SplitH;
+            assert_eq!(format_node(&node), "CONTAINER: SplitH")
+        }
     }
 }
